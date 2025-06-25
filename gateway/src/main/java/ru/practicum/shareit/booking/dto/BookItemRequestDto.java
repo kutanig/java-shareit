77cc8cee3635bb51c0b05ee5224fbd95221221c6
kutanig <anatolijkutarev@gmail.com>
2025-06-26 00:07:13 +0200

    Revert "hw16_server_and_gateway_v0.3"
    This reverts commit cf15199a

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -1,13 +1,13 @@
 package ru.practicum.shareit.booking.dto;
 
+import java.time.LocalDateTime;
+
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
